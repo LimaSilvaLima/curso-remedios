f4535780e15e316118cbb52d83fc2d4244eb325e
LimaSilvaLima <147210553+LimaSilvaLima@users.noreply.github.com>
2024-12-05 16:16:07 -0300

    criado filtro de segurança

---
+++ src/main/java/com/remedios/infra/SecurityFilter.java
@@ -0,0 +1,47 @@
+package com.remedios.infra;
+import com.remedios.infra.TokenService;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter{
+
+    @Autowired
+    private TokenService tokenService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        var tokenJWT = recuperarToken(request);
+        
+        var subject = tokenService.getSubject(tokenJWT);
+        
+
+        
+                
+                filterChain.doFilter(request, response);
+            }
+            
+            //System.out.println(tokenJWT);
+        
+            
+            private String recuperarToken(HttpServletRequest request) {
+                var authorizationHeader = request.getHeader("Authorization");
+                if (authorizationHeader == null) {
+                    throw new RuntimeException("Token invalido ou expirado");
+                }
+                return authorizationHeader;
+                
+            }
+
+
+}
--- src/main/java/com/remedios/infra/TokenService.java
+++ src/main/java/com/remedios/infra/TokenService.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.remedios.usuarios.Usuario;
 
 
@@ -31,6 +32,24 @@ public class TokenService {
         } catch (JWTCreationException exception){
             throw new RuntimeException("Erro ao gerar token", exception);
         }
+    }
+
+    public String getSubject(String TokenJWT){
+
+        try {
+            var algorithm = Algorithm.HMAC256(secret);
+        return JWT.require(algorithm)
+            .withIssuer("Remedios_api")
+            .build()
+            .verify(TokenJWT)
+            .getSubject();
+            
+        
+        } catch (JWTVerificationException exception){
+            throw new RuntimeException("Erro ao gerar token", exception);  
+}
+
+
     }
 
     private Instant dataExpiracao() {
