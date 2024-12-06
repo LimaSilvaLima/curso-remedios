ad04e28979d477c6efa1ff2a9234d814b414b167
LimaSilvaLima <147210553+LimaSilvaLima@users.noreply.github.com>
2024-12-06 09:53:16 -0300

    testes de autenticacao com token conclusivos

--- src/main/java/com/remedios/infra/SecurityConfiguration.java
+++ src/main/java/com/remedios/infra/SecurityConfiguration.java
@@ -1,20 +1,28 @@
 package com.remedios.infra;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UserDetailsRepositoryReactiveAuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
+
+    @Autowired
+    private SecurityFilter securityFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
@@ -23,7 +31,8 @@ public class SecurityConfiguration {
             .and().authorizeHttpRequests()
             .requestMatchers(HttpMethod.POST,  "/login").permitAll()
             .anyRequest().authenticated()
-            .and().build();
+            .and().addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
+            .build();
     }
 
     @Bean
--- src/main/java/com/remedios/infra/SecurityFilter.java
+++ src/main/java/com/remedios/infra/SecurityFilter.java
@@ -26,31 +26,28 @@ public class SecurityFilter extends OncePerRequestFilter{
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
+        
         var tokenJWT = recuperarToken(request);
+        
         if (tokenJWT != null) {
             var subject = tokenService.getSubject(tokenJWT);
             var usuario = repository.findByLogin(subject);
-
+             
             var autentication = new UsernamePasswordAuthenticationToken(usuario, null, usuario.getAuthorities());
-            
             SecurityContextHolder.getContext().setAuthentication(autentication);
 
         }
-
-        
-        
-                
-                filterChain.doFilter(request, response);
-            }
+        filterChain.doFilter(request, response);
+    }
                                  
-            private String recuperarToken(HttpServletRequest request) {
-                var authorizationHeader = request.getHeader("Authorization");
-                if (authorizationHeader != null) {
-                    return authorizationHeader;
-                }
-                return null;
-                
+        private String recuperarToken(HttpServletRequest request) {
+            var authorizationHeader = request.getHeader("Authorization");
+            if (authorizationHeader != null) {
+                return authorizationHeader.replace("Bearer ","");
             }
+        return null;
+                
+        }
 
 
 }
